e27fcd0f1d601d1003a67fd869a24cc748dc63c0
stanlocktop <wlad-test@ukr.net>
2023-09-07 21:54:34 +0300

    task 2 finish and  add to console Console.ForegroundColor

--- Lesson_3/Task2/Program.cs
+++ Lesson_3/Task2/Program.cs
@@ -1,14 +1,35 @@
-﻿namespace Task1
-
+﻿namespace Task2
 {
-    //1) Дано тризначне число.Знайти число, отримане під час прочитання його цифр справа наліво.
+   //2) Необхідно написати консольну програму, де користувач вводитиме з клавіатури 2 числа.Числа будуть порівнюватися з наступним виведенням в консоль результату цього порівняння(чи рівні значення, а якщо ні, яке число більше/менше).
     public class Example
     {
         public static void Main()
         {
             Console.OutputEncoding = System.Text.Encoding.Unicode;
-            Console.WriteLine("Введіть тризначне число, яке хочете прочитати справа наліво: ");
-            Console.WriteLine(string.Concat(Console.ReadLine().Reverse()));
+            Console.Write("Введіть 1 число: ");
+            double num1 = double.Parse(Console.ReadLine());
+
+            Console.Write("Введіть 2 число: ");
+            double num2 = double.Parse(Console.ReadLine());
+
+            if (num1 == num2)
+            {
+                Console.WriteLine("Числа рівні");
+            }
+            else 
+            if (num1 > num2)
+            {
+                Console.ForegroundColor = ConsoleColor.Green; // устанавливаем цвет
+                
+                Console.WriteLine($"Число {num1} більше за число {num2}!");
+                Console.ResetColor(); // сбрасываем в стандартный
+            }
+            else
+            {
+                Console.ForegroundColor = ConsoleColor.Red; // устанавливаем цвет
+                Console.WriteLine($"Число {num1} менше за число {num2}!");
+                Console.ResetColor(); // сбрасываем в стандартный
+            }
             Console.ReadKey();
         }
     }
