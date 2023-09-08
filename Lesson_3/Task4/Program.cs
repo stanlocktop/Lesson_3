cf9bc43a8a2640e356e7c444ac06ceb0d107d039
stanlocktop <wlad-test@ukr.net>
2023-09-08 17:44:20 +0300

    update task4

--- Lesson_3/Task4/Program.cs
+++ Lesson_3/Task4/Program.cs
@@ -2,37 +2,48 @@
 
 class Program
 {
-    static void Main(string[] args)
+    static void Main()
     {
-        Console.OutputEncoding = System.Text.Encoding.Unicode;
-        Console.Write("Введіть номер місяця від 1 до 12: ");
+        Console.Write("Введіть номер місяця (1-12): ");
         int month = int.Parse(Console.ReadLine());
 
-        string season = GetSeason(month);
+        string season;
 
-        if (season != "")
+        if (month >= 1 && month <= 12)
         {
+            switch (month)
+            {
+                case 1:
+                case 2:
+                case 12:
+                    season = "Зима";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    season = "Весна";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "Літо";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "Осінь";
+                    break;
+                default:
+                    season = "Немає такого місяця на цій планеті";
+                    break;
+            }
+
             Console.WriteLine($"Сезон: {season}");
         }
         else
         {
-            Console.WriteLine("Немає такого місяця на цій планеті :(");
+            Console.WriteLine("Номер місяця має бути від 1 до 12.");
         }
-    }
-
-    static string GetSeason(int month)
-    {
-        if (month >= 1 && month <= 12)
-        {
-            if (month == 12 || month <= 2)
-                return "Зима";
-            else if (month <= 5)
-                return "Весна";
-            else if (month <= 8)
-                return "Літо";
-            else
-                return "Осінь";
-        }
-        return "";
+        Thread.Sleep(100000);
     }
 }
