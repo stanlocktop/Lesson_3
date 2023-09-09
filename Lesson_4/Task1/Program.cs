b815dc15020ecc42cbe3ace7ed3a923cfe393fa2
stanlocktop <wlad-test@ukr.net>
2023-09-09 23:17:50 +0300

    update lesson

---
+++ Lesson_4/Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task1
+{
+    //    1) Напишіть програму, яка «запитуватиме» правильний пароль, доки він не буде введений. Правильний пароль нехай буде "root". Якщо пароль неправильний, програма повинна сказати "Неправильний пароль!"
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.Unicode;
+            string truePassword = "root";
+            string enteredPassword;
+            
+            do
+            { Console.WriteLine("Введіть пароль")
+                    }
+
+            Console.ReadKey();
+        }
+    }
+}
